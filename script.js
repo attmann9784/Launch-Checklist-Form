8412d645e47e47bd49a0add28ac124e4c590dfb9
attmann9784 <matthew_manness@cable.comcast.com>
2021-06-11 15:49:12 -0400

    final prodcut???

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ window.addEventListener("load", function() {
    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
       response.json().then(function(json) {
          const div = document.getElementById("missionTarget");
-         const planet = 0;
+         const planet = Math.round(Math.random()*5);
          div.innerHTML = `
          <h2>Mission Destination</h2>
          <ol>
